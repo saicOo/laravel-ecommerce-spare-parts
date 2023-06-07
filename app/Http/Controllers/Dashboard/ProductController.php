8a472113479bde88c0e2b5a23ddb9896878d57b1
unknown <sayed.ahmed.elbadry@gmail.com>
2023-06-07 17:27:24 +0200

    done

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -22,7 +22,6 @@ class ProductController extends Controller
     public function __construct()
     {
         $this->middleware('auth:admin');
-
     }
 
     public function index(Request $request)
--- resources/views/dashboard/purchases/show.blade.php
+++ resources/views/dashboard/purchases/show.blade.php
@@ -2,8 +2,10 @@
 @push('css')
     <style>
         @media print {
-            button.btn-print {display: none;}
-      }
+            button.btn-print {
+                display: none;
+            }
+        }
     </style>
 @endpush
 @section('content')
@@ -23,132 +25,140 @@
         <div class="container-fluid">
             <div class="row justify-content-between mb-3">
                 <div class="col-12 ">
-                     <h2 class="page-heading">{{ __('site.invoice') }} {{ __('site.purchases') }}</h2>
+                    <h2 class="page-heading">{{ __('site.invoice') }} {{ __('site.purchases') }}</h2>
                 </div>
             </div>
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header" >
-                        <div class="float-left">
-                            <div class="bootstrap-label">
-                                <span class="label label-{{$purchase->active == 1 ? 'success' : 'light'}}">{{$purchase->active == 0 ? __('site.draft') : __('site.active')}}</span>
-                                @if ($purchase->active == 1)
-                                <span class="label label-{{($purchase->payment_status == 3 ? 'success' : ($purchase->payment_status == 2 ? 'warning' : 'danger'))}}">{{$purchase->status}}</span>
-                                @endif
-
-                            </div>
-                        </div>
-                        <div class="float-right">
-                            <div class="button-group">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-outline-primary" onclick="printDiv()">{{__('site.print')}}</button>
-                                    @if ($purchase->active == 0)
-                                    <button type="submit" form="formPurchasesActive" class="btn btn-outline-primary" {{ $purchase->is_active == 1 ?'disabled' : ''}}>{{__('site.approval')}}</button>
-                                    @else
-                                    <a href="{{route('dashboard.purchases.edit',$purchase->id)}}" class="btn btn-outline-primary">{{__('site.payment')}}</a>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="float-left">
+                                <div class="bootstrap-label">
+                                    <span
+                                        class="label label-{{ $purchase->active == 1 ? 'success' : 'light' }}">{{ $purchase->active == 0 ? __('site.draft') : __('site.active') }}</span>
+                                    @if ($purchase->active == 1)
+                                        <span
+                                            class="label label-{{ $purchase->payment_status == 3 ? 'success' : ($purchase->payment_status == 2 ? 'warning' : 'danger') }}">{{ $purchase->status }}</span>
                                     @endif
+
                                 </div>
                             </div>
-                            <form action="{{route('dashboard.purchases.active', $purchase->id)}}" method="post" style="display: none" id="formPurchasesActive">
-                                @csrf
-                            </form>
-                        </div>
+                            <div class="float-right">
+                                <div class="button-group">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-outline-primary"
+                                            onclick="printDiv()">{{ __('site.print') }}</button>
+                                        @if ($purchase->active == 0)
+                                            <button type="submit" form="formPurchasesActive" class="btn btn-outline-primary"
+                                                {{ $purchase->is_active == 1 ? 'disabled' : '' }}>{{ __('site.approval') }}</button>
+                                        @else
+                                            <a href="{{ route('dashboard.purchases.edit', $purchase->id) }}"
+                                                class="btn btn-outline-primary">{{ __('site.payment') }}</a>
+                                        @endif
+                                    </div>
+                                </div>
+                                <form action="{{ route('dashboard.purchases.active', $purchase->id) }}" method="post"
+                                    style="display: none" id="formPurchasesActive">
+                                    @csrf
+                                </form>
+                            </div>
 
+                        </div>
                     </div>
                 </div>
             </div>
+
             <div class="row" id="print">
                 <div class="col-12">
-                    <div class="p-3 bg-white rounded">
-                        <div class="row">
-                            <div class="col-md-3">
-                                <h4 class="text-uppercase">@lang('site.invoice') @lang('site.purchases')</h4>
-                                <div class="billed"><span
-                                        class="font-weight-bold text-uppercase">@lang('site.invoice_no'):</span><span
-                                        class="ml-1">#{{ $purchase->invoice_no }}</span></div>
-                                <div class="billed"><span
-                                        class="font-weight-bold text-uppercase">@lang('site.date'):</span><span
-                                        class="ml-1">{{ $purchase->updated_at->format('M d, Y') }}</span></div>
-                                <div class="billed"><span
-                                        class="font-weight-bold text-uppercase">@lang('site.type'):</span><span
-                                        class="ml-1">{{ $purchase->type }}</span></div>
-                                <div class="billed"><span
-                                        class="font-weight-bold text-uppercase">@lang('site.status'):</span><span
-                                        class="ml-1">{{ $purchase->status }}</span></div>
-                            </div>
-                            <div class="col-md-3">
-                                <h4 class="text-uppercase">@lang('site.from')</h4>
-                                <div class="billed"><span
-                                        class="font-weight-bold text-uppercase">@lang('site.name'):</span><span
-                                        class="ml-1">{{ $purchase->supplier->name }}</span></div>
-                                <div class="billed"><span
-                                        class="font-weight-bold text-uppercase">@lang('site.phone'):</span><span
-                                        class="ml-1">{{ $purchase->supplier->phone }}</span></div>
-
-                            </div>
-                            <div class="col-md-3">
-                                <h4 class="text-uppercase">@lang('site.to')</h4>
-                                <div class="billed"><span
-                                        class="font-weight-bold text-uppercase">@lang('site.name'):</span><span
-                                        class="ml-1">{{ $setting->name }}</span></div>
-                                <div class="billed"><span
-                                        class="font-weight-bold text-uppercase">@lang('site.phone'):</span><span
-                                        class="ml-1">{{ $setting->phone }}</span></div>
-                                <div class="billed"><span
-                                        class="font-weight-bold text-uppercase">@lang('site.address'):</span><span
-                                        class="ml-1">{{ $setting->address }}</span></div>
-                            </div>
-                            <div class="col-md-3 text-right mt-3">
-                                <h4 class="text-primary mb-0">{{ $setting->name }}</h4><span>sparte-parts.com</span>
-                            </div>
-                        </div>
-                        <div class="mt-3">
-                            <div class="table-responsive">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>SKU</th>
-                                            <th>@lang('site.product')</th>
-                                            <th>@lang('site.price')</th>
-                                            <th>@lang('site.quantity')</th>
-                                            <th>@lang('site.total')</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($purchase->products as $index => $product)
-                                            <tr>
-                                                <td>#{{ $product->id }}</td>
-                                                <td><a
-                                                        href="{{ route('dashboard.products.show', $product->id) }}">{{ $product->name }}</a>
-                                                </td>
-                                                <td>${{ number_format($product->pivot->price, 2) }}</td>
-                                                <td>{{ $product->pivot->quantity }}</td>
-                                                <td>${{ number_format($product->pivot->price * $product->pivot->quantity, 2) }}
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-
-                                </table>
-                            </div>
+                    <div class="card">
+                        <div class="card-body">
                             <div class="row">
-                                <div class="col-lg-4 col-sm-5">
+                                <div class="col-md-3">
+                                    <h4 class="text-uppercase">@lang('site.invoice') @lang('site.purchases')</h4>
+                                    <div class="billed"><span
+                                            class="font-weight-bold text-uppercase">@lang('site.invoice_no'):</span><span
+                                            class="ml-1">#{{ $purchase->invoice_no }}</span></div>
+                                    <div class="billed"><span
+                                            class="font-weight-bold text-uppercase">@lang('site.date'):</span><span
+                                            class="ml-1">{{ $purchase->updated_at->format('M d, Y') }}</span></div>
+                                    <div class="billed"><span
+                                            class="font-weight-bold text-uppercase">@lang('site.type'):</span><span
+                                            class="ml-1">{{ $purchase->type }}</span></div>
+                                    <div class="billed"><span
+                                            class="font-weight-bold text-uppercase">@lang('site.status'):</span><span
+                                            class="ml-1">{{ $purchase->status }}</span></div>
+                                </div>
+                                <div class="col-md-3">
+                                    <h4 class="text-uppercase">@lang('site.from')</h4>
+                                    <div class="billed"><span
+                                            class="font-weight-bold text-uppercase">@lang('site.name'):</span><span
+                                            class="ml-1">{{ $purchase->supplier->name }}</span></div>
+                                    <div class="billed"><span
+                                            class="font-weight-bold text-uppercase">@lang('site.phone'):</span><span
+                                            class="ml-1">{{ $purchase->supplier->phone }}</span></div>
+
+                                </div>
+                                <div class="col-md-3">
+                                    <h4 class="text-uppercase">@lang('site.to')</h4>
+                                    <div class="billed"><span
+                                            class="font-weight-bold text-uppercase">@lang('site.name'):</span><span
+                                            class="ml-1">{{ $setting->name }}</span></div>
+                                    <div class="billed"><span
+                                            class="font-weight-bold text-uppercase">@lang('site.phone'):</span><span
+                                            class="ml-1">{{ $setting->phone }}</span></div>
+                                    <div class="billed"><span
+                                            class="font-weight-bold text-uppercase">@lang('site.address'):</span><span
+                                            class="ml-1">{{ $setting->address }}</span></div>
+                                </div>
+                                <div class="col-md-3 text-right mt-3">
+                                    <h4 class="text-primary mb-0">{{ $setting->name }}</h4><span>sparte-parts.com</span>
+                                </div>
+                            </div>
+                            <div class="mt-3">
+                                <div class="table-responsive">
                                     <table class="table">
-                                        <tr>
-                                            <th>@lang('site.total')</th>
-                                            <td>${{ number_format($purchase->total_price, 2) }}</td>
-                                        </tr>
-                                        <tr>
-                                            <th>@lang('site.amount_paid')</th>
-                                            <td>${{ number_format($purchase->amount_paid, 2) }}</td>
-                                        </tr>
+                                        <thead>
+                                            <tr>
+                                                <th>SKU</th>
+                                                <th>@lang('site.product')</th>
+                                                <th>@lang('site.price')</th>
+                                                <th>@lang('site.quantity')</th>
+                                                <th>@lang('site.total')</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($purchase->products as $index => $product)
+                                                <tr>
+                                                    <td>#{{ $product->id }}</td>
+                                                    <td><a
+                                                            href="{{ route('dashboard.products.show', $product->id) }}">{{ $product->name }}</a>
+                                                    </td>
+                                                    <td>${{ number_format($product->pivot->price, 2) }}</td>
+                                                    <td>{{ $product->pivot->quantity }}</td>
+                                                    <td>${{ number_format($product->pivot->price * $product->pivot->quantity, 2) }}
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+
                                     </table>
                                 </div>
-                                <div class="col-lg-4 col-sm-5"></div>
+                                <div class="row">
+                                    <div class="col-lg-4 col-sm-5">
+                                        <table class="table">
+                                            <tr>
+                                                <th>@lang('site.total')</th>
+                                                <td>${{ number_format($purchase->total_price, 2) }}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>@lang('site.amount_paid')</th>
+                                                <td>${{ number_format($purchase->amount_paid, 2) }}</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                    <div class="col-lg-4 col-sm-5"></div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="text-right mb-3">
-
                         </div>
                     </div>
                 </div>
